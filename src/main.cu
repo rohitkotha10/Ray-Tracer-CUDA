cc946cdb53cb7636cb44d919589c607eb665f60f
Rohit Kotha <rohitkotha10@gmail.com>
2022-12-14 22:07:05 +0530

    Add project files.

---
+++ src/main.cu
@@ -0,0 +1,43 @@
+﻿#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <iostream>
+#include <math.h>
+
+__global__ void add(int n, float *x, float *y) {
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
+    for (int i = index; i < n; i += stride) y[i] = x[i] + y[i];
+}
+
+int main(void) {
+    int N = 1 << 20;  // 1M elements
+
+    float *x;
+    float *y;
+
+    cudaMallocManaged(&x, N * sizeof(float));
+    cudaMallocManaged(&y, N * sizeof(float));
+
+    // initialize x and y arrays on the host
+    for (int i = 0; i < N; i++) {
+        x[i] = 1.0f;
+        y[i] = 8.0f;
+    }
+
+    // Run kernel on 1M elements on the CPU
+    int blockSize = 256;
+    int numBlocks = (N + blockSize - 1) / blockSize;
+    add<<<numBlocks, blockSize>>>(N, x, y);
+
+    cudaDeviceSynchronize();
+    // Check for errors (all values should be 3.0f)
+    float maxError = 0.0f;
+    for (int i = 0; i < N; i++) maxError = fmax(maxError, fabs(y[i] - 3.0f));
+    std::cout << "Max error: " << maxError << std::endl;
+
+    // Free memory
+    cudaFree(x);
+    cudaFree(y);
+
+    return 0;
+}
